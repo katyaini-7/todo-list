557166accc95f79d287b4eee1e602529122a47dd
Chilakala Katyaini Reddy <kathyaeenichilakala@gmail.com>
2026-09-04 16:17:14 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const taskInput = document.getElementById("taskInput");
+const addBtn = document.getElementById("addBtn");
+const taskList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", function () {
+
+    const taskText = taskInput.value.trim();
+
+    if (taskText === "") {
+        alert("Please enter a task!");
+        return;
+    }
+
+    const li = document.createElement("li");
+
+    li.textContent = taskText;
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+});
+
